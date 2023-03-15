5b3f4d4ef466737ac41432baa3e8d4c4d1e183e5
marina7lukianchuk <lukianchukmt@gmail.com>
2023-03-15 10:35:47 +0200

    commit

---
+++ assets/js/app.js
@@ -0,0 +1,55 @@
+
+const tabsBtn = $('.tabs_nav-btn');
+const tabsItems = $('.tabs_item');
+
+tabsBtn.each(function(index, item) {
+  $(item).click(function () {
+    let btn = $(this);
+    let tabItems = btn.attr("data-tab");
+    let tab = $(tabItems);
+
+    if (!btn.hasClass('active')) {
+      tabsBtn.removeClass('active');
+      tabsItems.removeClass('active');
+      btn.addClass('active');
+      tab.addClass('active');
+    }
+  });
+});
+
+
+
+const stars = $('.rating_star');
+
+
+stars.each(function(index, item) {
+  let i;
+  let starsLength = stars.length;
+  $(item).click(function () {
+    i = stars.index(item);
+   
+    if ($(item).hasClass('unchecked_star')) {
+      for (i; i >= 0; --i) {
+        $(stars[i]).addClass('checked_star').removeClass('unchecked_star');
+      }
+    }
+    else {
+      for (i; i < starsLength; ++i) {
+        $(stars[i]).addClass('unchecked_star').removeClass('checked_star');
+      }
+    }
+  })
+});  
+
+
+/* const raitNum = $('.ratingNum'); */
+/* const starsIndex = $(stars).attr('data-index'); */
+
+stars.each(function(index, item) {
+  const starsIndex = $(item).attr('data-index');
+  $(item).click(function() {
+    if ($(item).hasClass('checked_star')) {
+      $('.ratingNum').text(starsIndex);
+    }
+  });
+});
